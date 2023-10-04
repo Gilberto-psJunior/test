4e727491350a7dabb69e5b6035b528a9e3beb598
Gilberto-psJunior <121531207+Gilberto-psJunior@users.noreply.github.com>
2023-10-04 17:20:03 -0300

    2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const myModal = document.getElementById("myModal");
 const modal = document.querySelector("dialog");
 const buttonClose = document.querySelector("dialog button");
+const buttontrailer = document.querySelector(".reveal");
+let trailer = document.querySelector(".trailer");
+buttonReveal=document.querySelector(".reveal");
+let mediaClose = document.querySelector(".close");
 
 myModal.addEventListener("click", () => {
   modal.showModal();
@@ -9,3 +13,16 @@ myModal.addEventListener("click", () => {
 buttonClose.addEventListener("click", () => {
   modal.close();
 });
+// trailer do media query
+
+buttontrailer.addEventListener("click", () => {
+  document.querySelector(".trailer").style.display = "block";
+  document.querySelector(".reveal").style.display = "none";
+  document.querySelector(".close").style.display = "block";
+});
+mediaClose.addEventListener("click", () => {
+  document.querySelector(".trailer").style.display = "none";
+  document.querySelector(".close").style.display = "none";
+  document.querySelector(".reveal").style.display = "block";
+})
+
